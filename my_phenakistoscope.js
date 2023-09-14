ffa9d508672efc9d893a6c1757b8f344c51b2c25
bissetemil1 <140354171+bissetemil1@users.noreply.github.com>
2023-09-14 16:45:07 +1200

    commiting sorry!
    omg! sorry there are no updates! i forgot its not live updates unless you commit them, thought it was like google drive

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,49 +1,76 @@
-const SLICE_COUNT = 10;
+const SLICE_COUNT = 8;//6 minimum 18 max
 
 function setup_pScope(pScope){
   pScope.output_mode(ANIMATED_DISK);
   pScope.scale_for_screen(true);
-  pScope.draw_layer_boundaries(false);
-  pScope.set_direction(CCW);
+  pScope.draw_layer_boundaries(false);//turns lines off if false
+  pScope.set_direction(CCW);// clockwise CW counter clockwise CCW
   pScope.set_slice_count(SLICE_COUNT);
+  pScope.load_image_sequence("final" , "png", 9 );
+  pScope.load_image("ball" , "png");
+
 }
 
 function setup_layers(pScope){
 
-  new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
-
-  var layer1 = new PLayer(faces);
-  layer1.mode( SWIRL(5) );
-  layer1.set_boundary( 200, 1000 );
-
-  var layer2 = new PLayer(squares);
-  layer2.mode( RING );
-  layer2.set_boundary( 0, 400 );
-}
-
-function faces(x, y, animation, pScope){
+  new PLayer(null, 177, 230, 181);//pastel court green //lets us draw the whole circle background, ignoring the boundaries
   
-  scale(animation.frame*2);
+  var finalSequence = new PLayer(Ftennis); 
+  finalSequence.mode( SWIRL(4) ); // What i want for tennis racket
+  finalSequence.set_boundary( 200, 900 );
 
-  ellipse(0,0,50,50); // draw head
-  fill(30);
-  ellipse(-10,-10,10,10); //draw eye
-  ellipse(10,-10,10,10); // draw eye
-  arc(0,10,20,10,0,180); // draw mouth
+  var oval = new PLayer (petals);
+  oval.mode(SWIRL(3));
+  oval.set_boundary(100, 150);
+
+  var centreImage = new PLayer(launcher);
+  centreImage.mode (RING);
+  centreImage.set_boundary (100,200);
+
+  var outerRing = new PLayer(ring);
+  outerRing.mode (RING);
+  outerRing.set_boundary(850,900);
+
+  var innerRing = new PLayer(ringer);
+  innerRing.mode (RING);
+  innerRing.set_boundary(340,360);
+
+
 
 }
 
-function squares(x, y, animation, pScope){
+function petals(x, y, animation, pScope){
 
-  // this is how you set up a background for a specific layer
-  let angleOffset = (360 / SLICE_COUNT) / 2
-  let backgroundArcStart = 270 - angleOffset;
-  let backgroundArcEnd = 270 + angleOffset;
+  stroke(255,255,255)
+  ellipse(100, 0, 200, 100, animation.wave(5));
+  
+  }
+  
 
-  fill(66, 135, 245)
-  arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
-
-  fill(255)
-  rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
+function launcher(x, y, animation, pScope){
+  
+  scale(0.3);
+  pScope.draw_image("ball",-30,-400);
 
 }
+
+function Ftennis(x, y, animation, pScope){ 
+  scale (0.15);
+  pScope.draw_image_from_sequence("final", x, -900, animation.wave(2));
+
+  pScope.draw_image_from_sequence("final", 2000, -1000, animation.wave(2));
+}
+
+function ring(x, y, animation, pScope){
+
+  pScope.fill_background(255, 255, 255);
+
+}
+
+function ringer(x, y, animation, pScope){
+
+  pScope.fill_background(255, 255, 255);
+
+}
+
+
